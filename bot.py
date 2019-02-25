30a8260670adaf5e85ebc40365382132472569e0
Viki Berkle <vberkle@gmail.com>
2019-02-25 19:25:09 +0700

    delete line

--- bot.py
+++ bot.py
@@ -5,15 +5,17 @@ logging.basicConfig(format ='%(asctime)s - %(levelname)s - %(message)s',
                     level = logging.INFO,
                     filename = 'bot.log'
                     )
+
 def greet_user(bot, update):
     text = 'Вызван /start'
     update.message.reply_text(text)
+
 def talk_to_me(bot, update):
     user_text = "Привет {}! Ты написал {}".format(update.message.chat.first_name,update.message.text)
-    #print(user_text)
     logging.info("User: %s, Chat id: %s, Message: %s",
     update.message.chat.username,update.message.chat.id,update.message.text)
    update.message.reply_text(user_text)
+   
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs = settings.PROXY)
     logging.info('Бот запускается')
